99c620f4f0953eeae044fed4e602fca29d55a67c
Justin Chrisleigh M. Alqueza <chrisleighjustin@gmail.com>
2026-04-27 09:45:08 +0800

    Create Weapon.java

---
+++ Weapon.java
@@ -0,0 +1,11 @@
+public class Weapon {
+    String name;
+    int attackBonus;
+    int manaBonus; 
+
+    public Weapon(String name, int attackBonus, int manaBonus) {
+        this.name = name;
+        this.attackBonus = attackBonus;
+        this.manaBonus = manaBonus;
+    }
+}
